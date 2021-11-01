3f0be3d9849c57a1599a38add7ee19762059fd95
Nils K <git@nkn-it.de>
2021-11-01 14:30:50 +0100

    ms teams

---
+++ ms-teams.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+# Copyright 2021 Nils Knieling. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Send an Microsoft Teams message card via the Office connectors webhook API
+#
+# Help: https://github.com/Cyclenerd/notify-me
+
+BEGIN {
+	$VERSION = "1.0";
+}
+use utf8;
+binmode(STDOUT, ":utf8");
+use strict;
+use LWP::UserAgent;
+use HTTP::Request::Common;
+use JSON::XS;
+use App::Options (
+	option => {
+		url   => { required => 1, description => "Your Teams webhook url (example: https://outlook.office.com/webhook/<group>@<tenantID>/IncomingWebhook/<chars>/<guid>)", secure => 1 },
+		title => { required => 0, description => "Title" },
+		msg   => { required => 1, description => "Message" },
+	},
+);
+
+my $url = $App::options{url};
+# Create JSON for content body
+my %json;
+$json{title} = $App::options{title} if $App::options{title};
+$json{text}  = $App::options{msg};
+# Convert Perl hash to JSON
+my $json_text = encode_json \%json;
+
+my $ua = LWP::UserAgent->new;
+my $request = POST "$url";
+$request->authorization_basic($App::options{id}, $App::options{token});
+$request->header( 'Content-Type' => 'application/json', 'Content-Length' => length($json_text) );
+$request->content( $json_text );
+my $response = $ua->request($request);
+
+if ($response->is_success) {
+	print "OK: Message sent successfully.\n";
+} else {
+	warn "ERROR: Message could not be sent! Status: '". $response->status_line ."'\n";
+}
