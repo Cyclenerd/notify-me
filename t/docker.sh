9cfee10fc32f6073b47db1661f3e6d4b6c405c76
Nils K <git@nkn-it.de>
2021-11-02 07:30:32 +0100

    docker test script fix

--- t/docker.sh
+++ t/docker.sh
@@ -7,7 +7,7 @@ if ! docker run notify-me test.pl --version; then
 	exit 9
 fi
 
-for MY_PERL_SCRIPT in ../*.pl; do
+for MY_PERL_SCRIPT in *.pl; do
 	echo "» $MY_PERL_SCRIPT"
 	if ! docker run notify-me "$MY_PERL_SCRIPT" --version; then
 		exit 9
