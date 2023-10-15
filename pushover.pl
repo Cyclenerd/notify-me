4e5ae87c4b27750736c949fe7268a9e37991d7ea
Nils <git@cyclenerd.de>
2023-10-15 18:19:00 +0200

    pushover title and url

--- pushover.pl
+++ pushover.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Copyright 2021 Nils Knieling. All Rights Reserved.
+# Copyright 2021-2023 Nils Knieling. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -29,22 +29,34 @@ use Encode;
 use LWP::UserAgent;
 use App::Options (
 	option => {
-		user  => { required => 1, description => "The user/group key (not e-mail address) of your user" }, # viewable when logged into our dashboard
-		token => { required => 1, description => "Your application's API token", secure => 1 },
-		msg   => { required => 1, description => "Your message" },
+		user      => { required => 1, description => "The user/group key (not e-mail address) of your user [required]" },
+		token     => { required => 1, description => "Your application's API token [required]", secure => 1 },
+		msg       => { required => 1, description => "Your message [required]" },
+		title     => { required => 0, description => "Your message's title, otherwise your app's name is used" },
+		url       => { required => 0, description => "A URL to show with your message " },
+		url_title => { required => 0, description => "A title for the URL specified as the url parameter, otherwise just the URL is shown" },
 	},
 );
 
-my $user  = $App::options{user};
-my $token = $App::options{token};
-my $msg   = decode('UTF-8', $App::options{msg});
+my $user      = $App::options{user};
+my $token     = $App::options{token};
+my $msg       = decode('UTF-8', $App::options{msg});
+my $title     = decode('UTF-8', $App::options{title});
+my $url       = decode('UTF-8', $App::options{url});
+my $url_title = decode('UTF-8', $App::options{url_title});
 
-my $response = LWP::UserAgent->new()->post(
-	"https://api.pushover.net/1/messages.json", [
+my %post = (
 	"token"   => $token,
 	"user"    => $user,
 	"message" => $msg,
-]);
+);
+$post{'title'}     = $title if $title;
+$post{'url'}       = $url if $url;
+$post{'url_title'} = $url_title if $url_title;
+
+my $response = LWP::UserAgent->new()->post(
+	"https://api.pushover.net/1/messages.json", \%post
+);
 
 if ($response->is_success) {
 	print "OK: Message sent successfully.\n";
